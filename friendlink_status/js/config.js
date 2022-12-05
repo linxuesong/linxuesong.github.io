d5a599a9499e0ecd9a02b64706d2ec00523c6b81
linxuesong <lean_linxuesong@163.com>
2022-12-05 14:07:36 +0800

    Site updated: 2022-12-05 14:07:34

---
+++ friendlink_status/js/config.js
@@ -0,0 +1,21 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	// 检测谁就放睡的spcial monitor key
+	'm781564918-9921423871a83f5490f5cc8c', //blog
+	'm781564916-7faaae85ad735b3f13278edd', //tensuu
+	'm781564919-25b0b93dbda0fb2e2324fc72', //limi
+	'm787123122-05f956885a5775f6fcf1122f',//robot
+	'm781565595-9e794cddb584630daf12d8ae',//game
+	'm781565593-0cd3973dfb593cba55707cca',//stock
+	'm781565629-368d3c9acc3877ede7e53ff2',//app
+	'm781565634-cc63c456ca402fe8372fa416',//chongqing
+	'm781586472-909e1ec1f5e29f49fb24d815',//ss
+	'm781565858-17e02a8a091a8cd02f3d0474',//up a
+	'm781565855-a76d8043fdccb58e45a2c01b',//up b
+	'm781565889-ac54baedcaa28b74f4199e30',//db s
+	'm781565898-563a706c6b64e0b8965f746c'//db n
+
+];
+
+// refresh interval (in seconds)
+var __refresh = 360;
